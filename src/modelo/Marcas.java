92e9fa3e6ab665e881e2f826cfd9c7b43dbccca9
DanielSanchezSanchidrian <dnelsanchez@gmail.com>
2017-02-07 18:55:30 +0100

    marcas con metodos
    metodos de marcas añadidos

--- src/modelo/Marcas.java
+++ src/modelo/Marcas.java
@@ -14,121 +14,123 @@ import java.util.ArrayList;
  * @author juanxxiii
  */
 public class Marcas {
-    
+
     private int idMarca;
     private String nombre;
-    
 
     // <editor-fold defaultstate="collapsed" desc="Getters">
-    public int getIdMarca() {    
+    public int getIdMarca() {
         return idMarca;
     }
-    public String getNombre() {    
+
+    public String getNombre() {
         return nombre;
     }
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="Setter">
-     public void setIdMarca(int idMarca) {
+    public void setIdMarca(int idMarca) {
         this.idMarca = idMarca;
     }
-    public void setNombre(String nombre) {    
+
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
     // </editor-fold>
-    
+
     // <editor-fold defaultstate="collapsed" desc="Constructores">
     /**
      * Constructor vacio.
      */
     public Marcas() {
     }
-    
+
     /**
-     * 
+     *
      * @param idMarca
-     * @param nombre 
+     * @param nombre
      */
     public Marcas(int idMarca, String nombre) {
         this.idMarca = idMarca;
         this.nombre = nombre;
     }
-    
+
     /**
      * Constructor sin id para la creacion de marcas.
-     * @param nombre 
+     *
+     * @param nombre
      */
     public Marcas(String nombre) {
         this.nombre = nombre;
     }
     // </editor-fold>
-    
+
     // <editor-fold defaultstate="collapsed" desc="Metodos">
-  
-    public void crearCine() throws SQLException {
-        bd.actualizarTabla("insert into cine values(null, '"
-                + nombre + "', '"
-                + cif + "', '"
-                + direccion + "', '"
-                + poblacion + "', "
-                + cp + ")");
-    }
-
-   
-    public Cine mostrarCineId(int _idCine) throws SQLException {
-        Cine miCine = new Cine();
-        rs = bd.consultarTabla("select * from  cine where idCine = " + _idCine);
-        while (rs.next()) {
-            miCine.setIdCine(rs.getInt("idCine"));
-            miCine.setNombre(rs.getString("nombre_cin"));
-            miCine.setCif(rs.getString("cif_cin"));
-            miCine.setDireccion(rs.getString("direccion_cin"));
-            miCine.setPoblacion(rs.getString("poblacion_cin"));
-            miCine.setCp(rs.getInt("cp_cin"));
-        }
-        return miCine;
-    }
-
     
-    public static ArrayList<Cine> mostrarCines(ArrayList listaCines) throws SQLException {
+    /**
+     * Crea nueva marca.
+     *
+     * @throws SQLException
+     */
+    public void crearMarca() throws SQLException {
+        bd.actualizarTabla("insert into marca values(null, '" + nombre + ")");
+    }
+
+    /**
+     * 
+     * @param _idMarca
+     * @return
+     * @throws SQLException 
+     */
+    public Marcas mostrarMarcaId(int _idMarca) throws SQLException {
         ResultSet res;
-        Cine miCine = new Cine();
-        res = bd.consultarTabla("select * from cine");
-        listaCines.clear();
-        while(res.next()) {
-            miCine = new Cine();
-            miCine.setIdCine(res.getInt("idCine"));
-            miCine.setNombre(res.getString("nombre_cin"));
-            miCine.setCif(res.getString("cif_cin"));
-            miCine.setDireccion(res.getString("direccion_cin"));
-            miCine.setPoblacion(res.getString("poblacion_cin"));
-            miCine.setCp(res.getInt("cp_cin"));
-            listaCines.add(miCine);
+        Marcas miMarca = new Marcas();
+        res = bd.consultarTabla("select * from  marca where idMarca = " + _idMarca);
+        while (res.next()) {
+            miMarca.setNombre(res.getString("nombre"));
         }
-        System.out.println("hola 6");
-        return listaCines;
+        return miMarca;
+    }
+    
+    /**
+     * 
+     * @param listaMarcas
+     * @return
+     * @throws SQLException 
+     */
+    public static ArrayList<Marcas> mostrarMarcas(ArrayList listaMarcas) throws SQLException {
+        ResultSet res;
+        Marcas miMarca = new Marcas();
+        res = bd.consultarTabla("select * from marca");
+        listaMarcas.clear();
+        while (res.next()) {
+            miMarca = new Marcas();
+            miMarca.setNombre(res.getString("nombre"));
+
+            listaMarcas.add(miMarca);
+        }
+
+        return listaMarcas;
+    }
+    
+    /**
+     * 
+     * @param nombre
+     * @param _idMarca
+     * @throws SQLException 
+     */
+    public void modificarMarca(String nombre, int _idMarca) throws SQLException {
+        bd.actualizarTabla("Update marca set nombre = '"+ nombre + "' where idCine = " + _idMarca);
     }
 
-   
-    public void modificarMarca() throws SQLException {
-        bd.actualizarTabla("Update marca set nombre_cin = '"
-                + nombre + "', cif_cin = '"
-                + cif + "', direccion_cin = '"
-                + direccion + "', poblacion_cin = '"
-                + poblacion + "', cp_cin = "
-                + cp + " where idCine = " + idCine);
-    }
-    
-    
+    /**
+     * 
+     * @param _idMarca
+     * @throws SQLException 
+     */
     public void borrarMarcaID(int _idMarca) throws SQLException {
         bd.actualizarTabla("Delete from marca where idCine = " + _idMarca);
     }
 
-    
 // </editor-fold>
-    
-    
-    
-    
-    
 }
