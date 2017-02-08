dbb00d06ac833f034763942c3b0f0352ace66bc4
DanielSanchezSanchidrian <dnelsanchez@gmail.com>
2017-02-08 17:28:10 +0100

    core de conexion a la base de datos falta direccionamiento al servidor

---
+++ src/core/BDCore.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author adria
+ */
+public class BDCore {
+
+    Connection conexion = null;
+    Statement stmt = null;
+    String sql = null;
+    ResultSet rs = null;
+
+    /**
+     * Crea una conexion con una base de datos en Sqlite3. Si se usa este metodo
+     * tendremos que usar mas adelante el "desconectarBBDD"
+     *
+     * @param localizacionBBDD Ruta de la ubicacion de la base de datos
+     */
+    public void conectarSqlite(String localizacionBBDD) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost/prueba","dam2015", "dam2015");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(BDCore.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Problema para encontar la BBDD");
+        } catch (SQLException ex) {
+            Logger.getLogger(BDCore.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Con la BBDD");
+        }
+    }
+
+//   Codigo de muestra para hacer updates o consultas a la BBDD
+//        String mensaje = jtfSend.getText();
+//        Statement stmt = null;
+//        String sql = null;
+//        ResultSet rs = null;
+//            stmt = conexion.createStatement();
+//            stmt.executeUpdate("insert into juansapp values('" + nombre + "','" + mensaje + "')");
+//            sql = "Select * from juansapp";
+//            rs = stmt.executeQuery(sql);
+//            while (rs.next()) {
+//                jtaMensajes.append("\n" + rs.getString("Nombre")+": ");
+//                jtaMensajes.append(rs.getString("Mensaje"));
+//            }
+//            stmt.close();
+    /**
+     * Cierra la conexion con la base de datos, ya sea MySql o Sqlite3
+     *
+     */
+    public void desconectarBBDD() throws SQLException {
+        conexion.close();
+    }
+
+    /**
+     * Usado para consultar la BD
+     *
+     * @param _sql Comando para la consulta
+     * @return ResultSet con la informacion devuelta por la BD
+     * @throws SQLException Error al ejecutar la consulta
+     */
+    public ResultSet consultarTabla(String _sql) throws SQLException {
+        if (conexion == null) {
+            conectarSqlite("C:/bbdd/bbddCinesFree.db");
+        }
+        stmt = conexion.createStatement();
+        //Empezamos a trabajar con la BBDD
+        rs = stmt.executeQuery(_sql);
+        return rs;
+    }
+
+    public void prueba() throws SQLException {
+        if (conexion == null) {
+            conectarSqlite("C:/bbdd/bbddCinesFree.db");
+        }
+        stmt = conexion.createStatement();
+        //Empezamos a trabajar con la BBDD
+        rs = stmt.executeQuery("select * from cine");
+        while (rs.next()) {
+            System.out.println(rs.getInt("idCine"));
+            System.out.println(rs.getString("nombre_cin"));
+            System.out.println(rs.getString("cif_cin"));
+            System.out.println(rs.getString("direccion_cin"));
+            System.out.println(rs.getString("poblacion_cin"));
+            System.out.println(rs.getInt("cp_cin"));
+        }
+    }
+
+    /**
+     * Usado para hacer actualizaciones o deletes de la BD, no devuelve nada.
+     *
+     * @param _sql Comando para la actualizacion
+     * @throws SQLException Error al intentar hacer la actualizacion
+     */
+    public void actualizarTabla(String _sql) throws SQLException {
+        if (conexion == null) {
+            conectarSqlite("C:/bbdd/bbddCinesFree.db");
+        }
+        stmt = conexion.createStatement();
+        //Empezamos a trabajar con la BBDD
+        stmt.executeUpdate(_sql);
+    }
+
+    /**
+     * Metodo que comprueba los usuarios y contraseñas para permitir el acceso o
+     * denegarlo
+     *
+     * @param _user Usuario a buscar en la BD
+     * @param _pass Contraseña a buscar en la BD
+     * @return Devuelveun booleano, true si usery pas son correctas y false si
+     * no ha encontrado coincidencia
+     * @throws SQLException Error al buscar en la BD
+     */
+    public Boolean login(String _user, String _pass) throws SQLException {
+        Boolean acceso = false;
+        try { //Conseguimos los usuarios y contraseñas de la BD
+            rs = consultarTabla("select usuario_emp, contrasena_emp from empleado");
+        } catch (SQLException ex) {
+            System.out.println("Error al consultar usuarios y contraseñas en la BD");
+            Logger.getLogger(BDCore.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        while (rs.next()) {//Por cada resultado
+            if (_user.equals(rs.getString("usuario_emp"))) { //Si los usuarios coinciden
+                if (_pass.equals(rs.getString("contrasena_emp")) == true) { //Y las contraseñas tambien
+                    acceso = true; //Se puede entrar
+                }
+            }
+        }
+        return acceso;
+    }
+}
